201b7328067741a8dd02c0c5cad9eb9aa7d9e6be
eballci <emre-balci@outlook.com.tr>
2021-09-02 00:03:59 +0300

    Improve get-element unit

--- src/util/get-element.js
+++ src/util/get-element.js
@@ -7,7 +7,11 @@
 export const getElement = (selector) => {
     if (typeof selector === "string") return document.querySelector(selector)
 
+    if (typeof selector === "function") return selector()
+
     if (selector instanceof HTMLElement) return selector
 
-    throw new TypeError("Type of element selector have to be string.")
+    throw new TypeError(
+        "Type of element selector have to be string, function or HTMLElement."
+    )
 }
